b960cf845e361dc260297218a5b3ebf6ad29e59b
zed-amb <115375480+zed-amb@users.noreply.github.com>
2023-02-28 21:12:59 -0600

    yes

--- CS305_JS_OOP/W1D2Functions/Prog_Q1.js
+++ CS305_JS_OOP/W1D2Functions/Prog_Q1.js
@@ -14,29 +14,30 @@ const prompt = require("prompt-sync")();
 
 
 function computeSalesCommission(isSalaried, salesAmount) {
-    let commission = 0;
+  let commission = 0;
   
-    if (isSalaried) {
-      if (salesAmount > 500) {
-        commission = 0.01 * 500 + 0.02 * (salesAmount - 500);
-      } else if (salesAmount >= 300) {
-        commission = 0.01 * salesAmount;
-      }
-    } else {
-      if (salesAmount > 500) {
-        commission = 0.02 * 500 + 0.03 * (salesAmount - 500);
-      } else if (salesAmount >= 300) {
-        commission = 0.02 * salesAmount;
-      }
+  if (isSalaried) {
+    if (salesAmount >= 300 && salesAmount < 500) {
+      commission = salesAmount * 0.01;
+    } else if (salesAmount >= 500) {
+      commission = 500 * 0.01 + (salesAmount - 500) * 0.02;
     }
-  
-    return commission;
+  } else {
+    if (salesAmount >= 300 && salesAmount < 500) {
+      commission = salesAmount * 0.02;
+    } else if (salesAmount >= 500) {
+      commission = 500 * 0.02 + (salesAmount - 500) * 0.03;
+    }
   }
   
-  console.log("expect 0: ", computeSalesCommission(true, 200));
-  console.log("expect 0: ", computeSalesCommission(false, 200));
-  console.log("expect 3: ", computeSalesCommission(true, 300));
-  console.log("expect 6: ", computeSalesCommission(false, 300));
-  console.log("expect 65: ", computeSalesCommission(true, 3500));
-  console.log("expect 100: ", computeSalesCommission(false, 3500));
+  return commission;
+}
+
+console.log("expect 0: ", computeSalesCommission(true, 200));
+console.log("expect 0: ", computeSalesCommission(false, 200));
+console.log("expect 3: ", computeSalesCommission(true, 300));
+console.log("expect 6: ", computeSalesCommission(false, 300));
+console.log("expect 65: ", computeSalesCommission(true, 3500));
+console.log("expect 100: ", computeSalesCommission(false, 3500));
+
   
---
+++ CS305_JS_OOP/W1D2Functions/Prog_Q3.js
@@ -0,0 +1,37 @@
+"use strict"
+const prompt = require("prompt-sync")();
+
+/*5. Cost of House Down Payment
+Make a defining table and then write a program that calculates down payment for a home loan based
+on following rules. Your program should prompt for the cost and write the down payment amount to
+the console.
+Cost of House                          Down Payment
+$0 to less than 50K                      5% of the cost
+$50K to less than 100K                   $1000 + 10% of (cost - $50K)
+$100K to less than 200K                  $2000 + 15% of (cost - $100K)
+$200K and above                          $5000 + 10% of (cost - $200K)*/
+
+
+
+function calcDownpayment(costOfTheHouse) {
+    let downPayment;
+    if (costOfTheHouse > 0 && costOfTheHouse < 50000) {
+      downPayment = costOfTheHouse * 0.05;
+    } else if (costOfTheHouse >= 50000 && costOfTheHouse < 100000) {
+      downPayment = costOfTheHouse * 0.1;
+      downPayment += 1000;
+    } else if (costOfTheHouse >= 100000 && costOfTheHouse < 200000) {
+      downPayment = costOfTheHouse * 0.15;
+      downPayment += 2000;
+    } else {
+      downPayment = costOfTheHouse * 0.1;
+      downPayment += 5000;
+    }
+    return downPayment;
+  }
+  
+  console.log("expect 2000: ", calcDownpayment(40000));
+  console.log("expect 2500: ", calcDownpayment(50000));
+  console.log("expect 7500: ", calcDownpayment(100000));
+  console.log("expect 25000: ", calcDownpayment(250000));
+  
