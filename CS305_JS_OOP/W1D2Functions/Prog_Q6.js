b3c970c9a18e04db793af5b6f8fad39d384b6dfd
zed-amb <115375480+zed-amb@users.noreply.github.com>
2023-03-01 21:47:59 -0600

    Update Prog_Q6.js

--- CS305_JS_OOP/W1D2Functions/Prog_Q6.js
+++ CS305_JS_OOP/W1D2Functions/Prog_Q6.js
@@ -1,2 +1,15 @@
 "use strict"
 const prompt = require("prompt-sync")();
+
+/**
+ * Write a function that takes x and y co-ordinates of two points as inputs and returns the distance
+between these two points based on the formula, d = √( x 2 − x 1 ) 2 + ( y 2 − y 1 ) 2
+console.log("expect 7.07 : ", calcDistance (0, 0, 5, 5));
+ */
+
+function calcDistance(x1, y1, x2, y2) {
+    let distance = Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2));
+    return distance.toFixed(2);
+
+}
+console.log("expect 7.07 : ", calcDistance(0, 0, 5, 5));
