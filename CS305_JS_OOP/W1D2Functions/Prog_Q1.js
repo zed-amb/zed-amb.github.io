5f97bdf5baae19f3e8567dd3ffeccb5f337f68eb
zed-amb <115375480+zed-amb@users.noreply.github.com>
2023-03-02 06:43:13 -0600

    y

--- CS305_JS_OOP/W1D2Functions/Prog_Q1.js
+++ CS305_JS_OOP/W1D2Functions/Prog_Q1.js
@@ -1,5 +1,5 @@
 "use strict"
-const prompt = require("prompt-sync")();
+//const prompt = require("prompt-sync")();
 
 /* 1. Write a program to compute sales commission based on following rules:
 • If the salesman is salaried then
@@ -15,7 +15,7 @@ const prompt = require("prompt-sync")();
 
 export function computeSalesCommission(isSalaried, salesAmount) {
   let commission = 0;
-  
+
   if (isSalaried) {
     if (salesAmount >= 300 && salesAmount < 500) {
       commission = salesAmount * 0.01;
@@ -29,30 +29,9 @@ export function computeSalesCommission(isSalaried, salesAmount) {
       commission = 500 * 0.02 + (salesAmount - 500) * 0.03;
     }
   }
-  
+
   return commission;
 }
-
-describe("test of ComputSalesCommission", function(){
-  it("tests salaried and 200 sales", function(){
-  assert.strictEqual(computeSalesCommission(true, 200), 0);
-  });
-  it("tests not salaried and 200 sales", function(){
-  assert.strictEqual(computeSalesCommission(false, 200), 0);
-  });
-  it("tests salaried and 300 sales", function(){
-  assert.strictEqual(computeSalesCommission(true, 300), 3);
-  });
-  it("tests not salaried and 300 sales", function(){
-  assert.strictEqual(computeSalesCommission(false, 300), 6);
-  });
-  it("tests salaried and 3500 sales", function(){
-  assert.strictEqual(computeSalesCommission(true, 3500), 65);
-  });
-  it("tests not salaried and 3500 sales", function(){
-  assert.strictEqual(computeSalesCommission(false, 3500), 100);
-  });
- });
 
 console.log("expect 0: ", computeSalesCommission(true, 200));
 console.log("expect 0: ", computeSalesCommission(false, 200));
@@ -61,4 +40,3 @@ console.log("expect 6: ", computeSalesCommission(false, 300));
 console.log("expect 65: ", computeSalesCommission(true, 3500));
 console.log("expect 100: ", computeSalesCommission(false, 3500));
 
-  
