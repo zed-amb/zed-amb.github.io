1104f1fa91b858019fd0a3227b8eb87a7e111336
zed-amb <115375480+zed-amb@users.noreply.github.com>
2023-03-02 21:15:24 -0600

    tee

--- CS305_JS_OOP/W1D4Arrays/Assignment/Q1.js
+++ CS305_JS_OOP/W1D4Arrays/Assignment/Q1.js
@@ -1,21 +1,22 @@
 
 //Q1
-function maxOfThree(a, b, c){
-    if(a>b && a>c){
+function maxOfThree(a, b, c) {
+    if (a > b && a > c) {
         return a;
     }
-    else if(b>a && b>c){
+    else if (b > a && b > c) {
         return b;
     }
-    else if(c>a && c>b){
+    else if (c > a && c > b) {
         return c;
     }
-    else{
-    return "They are equal.";
-}
+    else {
+        return "They are equal.";
+    }
 }
 
-let num = maxOfThree(3,3,3);
+let num = maxOfThree(3, 3, 3);
 console.log(num);
 
 //Q2
+
