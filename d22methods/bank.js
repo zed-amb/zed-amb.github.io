5624049c4b188f5652d681de82f8e8860a8765b8
zed-amb <115375480+zed-amb@users.noreply.github.com>
2023-03-07 18:04:37 -0600

    update

--- d22methods/bank.js
+++ d22methods/bank.js
@@ -73,4 +73,4 @@ return balance;
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
 /* must be at end of file if are exporting an object so the export is after the definition */
-module.exports = {bank }; //add all of your object names here that you need for the node mocha tests
+//module.exports = {bank }; //add all of your object names here that you need for the node mocha tests
