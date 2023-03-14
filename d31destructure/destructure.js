efd1b7bfd4d1ad4159bc6fbab6f406b6453cf7e5
zed-amb <115375480+zed-amb@users.noreply.github.com>
2023-03-13 20:43:11 -0500

    Updated

---
+++ d31destructure/destructure.js
@@ -0,0 +1,16 @@
+/* eslint-disable require-jsdoc */
+
+
+
+export function topSalary(salaries){
+    let maxSalary = 0;
+    let maxName = null;
+    
+    for(let [name, salary] of Object.entries(salaries)){
+      if(maxSalary<salary){
+        maxSalary = salary;
+        maxName = name;
+      }
+    }
+    return maxName;
+    }
---
+++ d31destructure/destructureTests.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+/* comment out the import assert line when running in the browser */
+//import assert from "node:assert/strict"; 
+
+import { topSalary } from "./destructure.js";
+
+ 
+    
+/* 
+Create the function topSalary(salaries) that returns the name of the top-paid person.
+    If salaries is empty, it should return null.
+    If there are multiple top-paid persons, return any of them.
+P.S. Use Object.entries and destructuring to iterate over key/value pairs.
+*/
+describe("topSalary", function() {
+    it("returns top-paid person", function() {
+      let salaries = {
+        "John": 100,
+        "Pete": 300,
+        "Mary": 250
+      };
+  
+      assert.equal( topSalary(salaries), "Pete" );
+    });
+  
+    it("returns null for the empty object", function() {
+        console.log("topsalary: ", topSalary({}) );
+      assert.strictEqual( topSalary({}), null);
+    });
+  });
