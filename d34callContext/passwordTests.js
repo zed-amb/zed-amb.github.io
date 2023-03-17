c9a0ff38e8995edf605c358119add17a16f91430
zed-amb <115375480+zed-amb@users.noreply.github.com>
2023-03-16 21:58:55 -0500

    upd

--- d34callContext/passwordTests.js
+++ d34callContext/passwordTests.js
@@ -7,7 +7,7 @@ import { user, askPassword, user2, askPassword2  } from "./passwords.js";
 
 /* modify the book functions (in the Function binding section tasks) to return values instead of displaying alerts, and to take the passwords as parameters rather than prompting for them.  
 Then write the additional mocha tests to use call and apply instead of bind.
-*/
+*//* 
 describe("fix function that loses 'this'", function () {
     it("tests rockstar", function () {
       assert.strictEqual(askPassword.call(user, user.loginOk, user.loginFail, "rockstar"),"John logged in");
@@ -41,4 +41,64 @@ describe("fix function that loses 'this'", function () {
     it("tests wrong password", function () {
       assert.strictEqual(askPassword2(() => user2.login(true), () => user2.login(false), "1234"), "John failed to log in" );
     });
+  }); */
+/* 
+  describe("askPassword", function () {
+    it("tests call method with rockstar", function () {
+      assert.strictEqual(
+        askPassword(user.loginOk.call(user), user.loginFail.call(user), "rockstar"),
+        "John logged in"
+      );
+    });
+  
+    it("tests call method with wrong password", function () {
+      assert.strictEqual(
+        askPassword(user.loginOk.call(user), user.loginFail.call(user), "1234"),
+        "John failed to log in"
+      );
+    });
+  
+    it("tests apply method with rockstar", function () {
+      assert.strictEqual(
+        askPassword(user.loginOk.apply(user), user.loginFail.apply(user), "rockstar"),
+        "John logged in"
+      );
+    });
+  
+    it("tests apply method with wrong password", function () {
+      assert.strictEqual(
+        askPassword(user.loginOk.apply(user), user.loginFail.apply(user), "1234"),
+        "John failed to log in"
+      );
+    });
+  
+    it("tests bind method with rockstar", function () {
+      assert.strictEqual(
+        askPassword(user.loginOk.bind(user), user.loginFail.bind(user), "rockstar"),
+        "John logged in"
+      );
+    });
+  
+    it("tests bind method with wrong password", function () {
+      assert.strictEqual(
+        askPassword(user.loginOk.bind(user), user.loginFail.bind(user), "1234"),
+        "John failed to log in"
+      );
+    });
   });
+  
+  describe("askPassword2", function () {
+    it("tests password rockstar", function () {
+      assert.strictEqual(
+        askPassword2(user2.login.bind(user2, true), user2.login.bind(user2, false), "rockstar"),
+        "John logged in"
+      );
+    });
+  
+    it("tests wrong password", function () {
+      assert.strictEqual(
+        askPassword2(user2.login.bind(user2, true), user2.login.bind(user2, false), "1234"),
+        "John failed to log in"
+      );
+    });
+  }); */
